0dab8e53d394c80dbaf90b8af6c0c0a1e8661638
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2021-12-07 11:10:05 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// When the factButton is clicked...
+$("#factButton").on("click", function() {
+	// We Generate a random number between 0 and 4 (the number of facts in the curienFact)
+	var number = Math.floor((Math.random() * curienFacts.length));
+	// We display the fact from the CurienFactsArray that is in the random position we just generated.
+	$("#factText").text(curienFacts[number])
+})
+
+// This array holds all of our Curien facts!
+var curienFacts = ["Located in San Pedro California", "Shipping Policy: Will ship within 1 business day if item(s) are in stock", "Your Tech Guru: Joshua Carton", "Refund Policy: 90 refund on all items (excluding food items)"]
